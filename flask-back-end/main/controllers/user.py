0996254917694f4459e2e3487c2060d19f7fe330
john-1123 <johnwowhn@gmail.com>
2023-05-05 21:24:18 +0800

    modify user service and controller

--- flask-back-end/main/controllers/user.py
+++ flask-back-end/main/controllers/user.py
@@ -1,23 +1,25 @@
 from flask import request
 from flask_restful import Resource
-from main.services.user import get_all_users, get_user, create_user, update_user, delete_user
+from main.services.user import UserService
+
+service = UserService()
 
 class GetAllUser(Resource):
     def get(self):
-        return get_all_users()
+        return service.get_all()
 
 class GetUser(Resource):
     def get(self, id):
-        return get_user(user_id=id)
+        return service.get(user_id=id)
 
 class PostUser(Resource):
     def post(self):
-        return create_user(data=request.json)
+        return service.create(data=request.json)
 
 class UpdateUser(Resource):
     def put(self, id):
-        return update_user(user_id=id, data=request.json)
+        return service.update(user_id=id, data=request.json)
 
 class DeleteUser(Resource):
     def delete(self, id):
-        return delete_user(user_id=id)
+        return service.delete(user_id=id)
--- flask-back-end/main/services/user.py
+++ flask-back-end/main/services/user.py
@@ -4,47 +4,46 @@ from main.models.user import User
 from main.schemas.user import UserSchema
 import datetime
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+class UserService:
+    def __init__(self):
+        self.user_schema = UserSchema()
+        self.users_schema = UserSchema(many=True)
 
+    def get_all(self):
+        users = User.query.all()
+        return jsonify(self.users_schema.dump(users))
 
-def get_all_users():
-    users = User.query.all()
-    return jsonify(users_schema.dump(users))
+    def get(self, user_id):
+        user = User.query.get(user_id)
+        return self.user_schema.jsonify(user)
 
-def get_user(user_id):
-    user = User.query.get(user_id)
-    return user_schema.jsonify(user)
+    def create(self, data):
+        user = User.query.filter_by(email=data['email']).first()
+        if not user:
+            new_user = User(
+                email = data['email'],
+                password = data['password'],
+                username = data['username'],
+                address = data['address'],
+                phone = data['phone'],
+                registered_on = datetime.datetime.now()
+            )
+            save(new_user)
+            return self.user_schema.jsonify(new_user)
 
-def create_user(data):
-    user = User.query.filter_by(email=data['email']).first()
-    if not user:
-        new_user = User(
-            email = data['email'],
-            password = data['password'],
-            username = data['username'],
-            address = data['address'],
-            phone = data['phone'],
-            registered_on = datetime.datetime.now()
-        )
-        save(new_user)
-        return user_schema.jsonify(new_user)
+    def update(self, user_id, data):
+        user = User.query.get(user_id)
+        if user:
+            user.email = data['email'],
+            user.password = data['password'],
+            user.username = data['username'],
+            user.address = data['address'],
+            user.phone = data['phone'],
+            save(user)
+            return self.user_schema.jsonify(user)
 
-def update_user(user_id, data):
-    user = User.query.get(user_id)
-    if user:
-        user.email = data['email'],
-        user.password = data['password'],
-        user.username = data['username'],
-        user.address = data['address'],
-        user.phone = data['phone'],
-        save(user)
-        return user_schema.jsonify(user)
-
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if user:
-        delete(user)
-        return user_id
-
-
+    def delete(self, user_id):
+        user = User.query.get(user_id)
+        if user:
+            delete(user)
+            return user_id
