8329361b41467497aaa9352406127c2008bf0347
anny0807 <anny_2848@yahoo.com.tw>
2023-05-26 13:21:20 +0800

    services/product.py revise, models/product.py add cart_item relationship

--- flask-back-end/main/models/market.py
+++ flask-back-end/main/models/market.py
@@ -1,8 +1,16 @@
 from ._db import db
+from main.models.product import Product
 
 class Market(db.Model):
     __tablename__ = 'market'
-    pass
+    market_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    market_name = db.Column(db.String(20), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self):
-        pass
+    product = db.relationship('Product', backref='market')
+
+    def __init__(self, market_name, user_id, registered_on):
+        self.market_name = market_name
+        self.user_id = user_id
+        self.registered_on = registered_on
