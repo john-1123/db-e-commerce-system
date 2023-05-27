eb50342ae5be1708e76e706e35efd79fc7285482
anny0807 <anny_2848@yahoo.com.tw>
2023-05-28 01:00:19 +0800

    import CartItem

--- flask-back-end/main/models/product.py
+++ flask-back-end/main/models/product.py
@@ -1,5 +1,5 @@
 from ._db import db
-from sqlalchemy.orm import relationship
+# from main.models.cart_item import CartItem
 
 class Product(db.Model):
     __tablename__ = 'product'
@@ -10,7 +10,7 @@ class Product(db.Model):
     price = db.Column(db.Integer, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
     market_id = db.Column(db.Integer, db.ForeignKey("market.market_id"), nullable=False)
-    cart_item = db.relationship('cart_item', backref='product')
+    # cart_item = db.relationship('cart_item', backref='product')
 
     def __init__(self, product_name, category, brand, price, stock, market_id):
         self.product_name = product_name
@@ -19,4 +19,3 @@ class Product(db.Model):
         self.price = price
         self.stock = stock
         self.market_id = market_id
-        
