a54ba85cf0d0b25a1e2cd32ffa08a5cf8e13bc6f
109703008 <andy9110705@gmail.com>
2023-05-27 20:30:03 +0800

    update

--- vue3-front-end/src/views/Home.vue
+++ vue3-front-end/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <v-container class="text-center">
     <h1>Items</h1>
-    <v-row>
-        <v-col cols="12" sm="6" md="4" v-for="item in data.items" :key="item.id">
-          <v-card>
-            <v-card-title>{{ item.name }}</v-card-title>
-            <v-card-subtitle>{{ item.description }}</v-card-subtitle>
-            <v-card-actions>
-              <v-btn>Action 1</v-btn>
-              <v-btn>Action 2</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
+    <v-row no-gutters v-for="market in data.items" :key="market.id">
+      <v-col cols="12" md="12">
+        <v-chip class="ma-2" size="x-large">{{ market.market_name }}</v-chip>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        v-for="product in market.product_list.slice(0, 3)"
+        :key="product.id"
+      >
+        <v-card>
+          <v-card-title>{{ product.name }}</v-card-title>
+          <v-card-subtitle>{{ product.description }}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn>${{ product.price }}</v-btn>
+            <v-btn>Action 2</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -25,9 +35,58 @@ export default {
     const data = reactive({
       items: [
         {
-          id: 1,
-          name: "Item 1",
-          description: "This is item 1",
+          market_id: 1,
+          market_name: "MARKET NAME",
+          product_list: [
+            {
+              id: 1,
+              name: "Item 1",
+              description: "This is item 1",
+              price: 100,
+            },
+            {
+              id: 2,
+              name: "Item 2",
+              description: "This is item 2",
+              price: 100,
+            },
+            {
+              id: 3,
+              name: "Item 3",
+              description: "This is item 3",
+              price: 100,
+            },
+          ],
+        },
+        {
+          market_id: 2,
+          market_name: "MARKET NAME",
+          product_list: [
+            {
+              id: 1,
+              name: "Item 1",
+              description: "This is item 1",
+              price: 100,
+            },
+            {
+              id: 2,
+              name: "Item 2",
+              description: "This is item 2",
+              price: 100,
+            },
+            {
+              id: 3,
+              name: "Item 3",
+              description: "This is item 3",
+              price: 100,
+            },
+            {
+              id: 4,
+              name: "Item 4",
+              description: "This is item 4",
+              price: 500,
+            },
+          ],
         },
       ],
     });
@@ -35,7 +94,7 @@ export default {
   },
 
   data() {
-    return { };
+    return {};
   },
 };
 </script>
