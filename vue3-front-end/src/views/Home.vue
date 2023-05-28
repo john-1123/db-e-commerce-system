cc10857d3fc26743a1e2e84c876fc21d3cc6df3d
john-1123 <johnwowhn@gmail.com>
2023-05-28 14:15:07 +0800

    update home style

--- vue3-front-end/src/views/Home.vue
+++ vue3-front-end/src/views/Home.vue
@@ -1,24 +1,27 @@
 <template>
   <v-container class="text-center">
     <h1>Items</h1>
-    <v-row no-gutters v-for="market in data.items" :key="market.id">
-      <v-col cols="12" md="12">
-        <v-chip class="ma-2" size="x-large">{{ market.market_name }}</v-chip>
+    <v-row 
+      class="justify-center"
+      no-gutters v-for="market in data.items" :key="market.id">
+      <v-col cols="12" md="12" class="ma-3">
+        <v-chip size="large" append-icon="mdi-arrow-right">{{ market.market_name }}</v-chip>
       </v-col>
 
       <v-col
         cols="12"
         sm="6"
-        md="4"
-        v-for="product in market.product_list.slice(0, 3)"
+        md="3"
+        v-for="product in market.product_list"
         :key="product.id"
+        class="ma-3 text-start"
       >
         <v-card>
           <v-card-title>{{ product.name }}</v-card-title>
           <v-card-subtitle>{{ product.description }}</v-card-subtitle>
+          <v-card-title>Price: ${{ product.price }}</v-card-title>
           <v-card-actions>
-            <v-btn>${{ product.price }}</v-btn>
-            <v-btn>Action 2</v-btn>
+            <v-btn rounded="lg" variant="tonal" prepend-icon="mdi-cart">加入購物車</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -30,7 +33,7 @@
 import axios from "axios";
 import { computed, defineComponent, reactive, onMounted } from "vue";
 
-export default {
+export default defineComponent({
   setup() {
     const data = reactive({
       items: [
@@ -54,6 +57,18 @@ export default {
               id: 3,
               name: "Item 3",
               description: "This is item 3",
+              price: 100,
+            },
+            {
+              id: 4,
+              name: "Item 4",
+              description: "This is item 4",
+              price: 100,
+            },
+            {
+              id: 5,
+              name: "Item 5",
+              description: "This is item 5",
               price: 100,
             },
           ],
@@ -96,5 +111,5 @@ export default {
   data() {
     return {};
   },
-};
+});
 </script>
