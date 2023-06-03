35e7ab29426ab94eb6ecd69ce2a77e207022f8aa
john-1123 <johnwowhn@gmail.com>
2023-06-03 15:21:57 +0800

    fix bug

--- flask-back-end/main/models/market.py
+++ flask-back-end/main/models/market.py
@@ -1,5 +1,7 @@
 from ._db import db
-
+from main.models.product import Product
+# from main.models.cart import Cart
+# from main.models.cart_item import CartItem
 
 class Market(db.Model):
     __tablename__ = 'market'
@@ -7,10 +9,11 @@ class Market(db.Model):
     market_name = db.Column(db.String(20), unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
-    product = db.relationship('Product', backref='market')
+
     # cart = db.relationship('cart', backref='market')
     # cart_item = db.relationship('cart_item', backref='market')
-    
+    product = db.relationship('Product', backref='market')
+
     def __init__(self, market_name, user_id, registered_on):
         self.market_name = market_name
         self.user_id = user_id
